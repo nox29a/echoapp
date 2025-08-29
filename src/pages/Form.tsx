575bd30070cd3184b2108e7062349d9be2e42387
Łukasz Nox <lukasznox@MacBook-Pro-ukasz.local>
2025-08-29 16:57:48 +0200

    updatelat4

--- src/pages/Form.tsx
+++ src/pages/Form.tsx
@@ -81,7 +81,7 @@ export default function CoachingForm() {
     }
 
     try {
-      await fetch("https://nox29a.app.n8n.cloud/webhook-test/coaching-form", {
+      await fetch("https://nox29a.app.n8n.cloud/webhook/coaching-form", {
         method: "POST",
         body: formData,
       });
